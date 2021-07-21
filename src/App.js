46a9e5c2b8aa942c61e900b483eb6fc10362df0f
gwsy93 <giordansy@gmail.com>
2021-07-21 21:05:20 +0800

    Class Test

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          class =   "fdsfds"
         >
           Learn React
         </a>
